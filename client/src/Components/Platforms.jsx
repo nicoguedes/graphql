c1bafe47dbacbc68804f01f1e926a1dd2a57b26d
Vinícius Guedes <viniciusmpg@gmail.com>
2021-05-03 15:57:13 -0300

    listing platforms

--- client/src/Components/Platforms.jsx
+++ client/src/Components/Platforms.jsx
@@ -1,13 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { LOAD_PLATFORMS, LOAD_USERS } from "../GraphQL/Queries";
 
 export default () => {
   const { error, loading, data } = useQuery(LOAD_USERS);
+  const [platforms, setPlatforms] = useState([]);
 
   useEffect(() => {
-    console.log(data);
+    if (data && data.platform) {
+      setPlatforms(data.platform);
+      console.log(data.platform);
+    }
   }, [data]);
 
-  return <div></div>;
+  return (
+    <div>
+      {platforms &&
+        platforms.map((plat) => {
+          return <h3>{plat.name}</h3>;
+        })}
+    </div>
+  );
 };
